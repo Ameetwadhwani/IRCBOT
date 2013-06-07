a4ebe823b705eddef70e9d87f7f708f7bf741639
alicelara <hello@alicelara.ca>
2013-06-07 16:28:35 -0400

    switched to cinch IRC framework

--- irc_bot.rb
+++ irc_bot.rb
@@ -4,8 +4,8 @@ server = "chat.freenode.net"
 port = "6667"
 nick = "peekaboobot"
 channel = "#bitmaker"
-greeting_prefix = "privmsg #bitmaker :"  # what does the : denote?  
-greetings = ['Hello', 'Hi', 'Hola', 'Yo', 'Wazzup'] # these are the greetings the program is looking for
+greeting_prefix = "PRIVMSG #{channel} :"  # what does the : denote?  
+greetings = ["Hello", "Hi", "Hola", "Yo", "Wazzup"] # these are the greetings the program is looking for
 
 # opens a new port with the correct server and port 
 s = TCPSocket.open(server, port)
@@ -17,23 +17,45 @@ s.puts "USER peekaboobot 0 * peekaboobot" # -- this is creating a new user - we'
 
 s.puts "NICK #{nick}" # -- this is creating a nickname that is equal to our botname.
 s.puts "JOIN #{channel}" # -- this is connecting us to the channel we defined above. 
-s.puts "PRIVMSG #{channel} :Hello from peekaboobot" #  we think this is saying we're going to have a private message. We're not totally sure why hello starts with :Hello.  does the semi colon separate the message?  
+# s.puts "PRIVMSG #{channel} :we see you" #  we think this is saying we're going to have a private message. We're not totally sure why hello starts with :Hello.  does the semi colon separate the message?  
+s.puts greeting_prefix + 'we see you'
 
-until s.eof? do # until the server get to the end of file (EOF) - the ? denotes a boolean true or false.  
-	msg = s.gets.chomp # variable msg (message) = an input from 'chat.freenode.net' 
-	puts msg # output the message that was just captured. 
+s.puts greeting_prefix + "...and we're coming for you"
 
-	wasGreeted = false #
-	greetings.each do |g|  # for each instance of the variable greeting, do g where g is an element of greeting 
-		wasGreeted = true if msg.eql? g 
-	#end # ends each do 
+i = 0
+until s.eof?
+	puts "inside until loop...."
+	msg = s.gets.chomp.downcase
+	puts msg
 
-	if msg.include?(greeting_prefix) && wasGreeted
-		response = "BOO!"
-		s.puts "PRIVMSG #{channel} :#{response}"  # output the response to the channel
+	i = i + 1
+
+	if i == 1
+		puts "inside s.eof"
+		s.puts "artbot sucks - deal with it"
 	end
 end
 
 
+# def say_something(tcpsocket, message)
+# 	greetings_prefix + message
+# end
 
+# until s.eof? do # until the server get to the end of file (EOF) - the ? denotes a boolean  
+# 	msg = s.gets.chomp.downcase # variable msg (message) = an input from 'chat.freenode.net' 
+# 	puts msg # output the message that was just captured. 
 
+# 	was_greeted = false
+# 	greetings.each do |g|  # for each instance of the variable greeting, do g where g is an element of greeting 
+# 		was_greeted = true if msg.include? g 
+# 	end
+# 		# was_greeted = greetings.include? msg
+
+# 	if msg.include? greeting_prefix && was_greeted
+# 			response = "BOO!BOO!BOO!BOO!BOO!BOO!BOO!"
+
+# 			# say_something( s, "this is the actual message i want to send")
+
+# 			s.puts greeting_prefix + "#{response}"  # output the response to the channel
+# 	end 	
+# end
