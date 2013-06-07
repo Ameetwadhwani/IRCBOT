679037155b851b97955c65b5e501e4e2ae775ffb
alicelara <hello@alicelara.ca>
2013-06-07 17:33:18 -0400

    added Twitter.rb

--- cinch_original.rb
+++ cinch_original.rb
@@ -12,11 +12,5 @@ bot = Cinch::Bot.new do
   end
 end
 
-Twitter.configure do |config|
-  config.consumer_key = 7xKxgdC3pHcpHNyAV3BJg
-  config.consumer_secret = rwKC1IIM0t5xqKqZd41YVdwKmnV7tL2yQ70z3J8ugSc
-  config.oauth_token = 1491301814-53EGuYCKdNMpvt7BgoqDLZILfLN1aDsBKXZ4Tbv
-  config.oauth_token_secret = 1vDjxwgKNHNs988emcz9E3AYaH1YLEqzJb2avkb3I
-end
 
 bot.start
---
+++ twitter.rb
@@ -0,0 +1,8 @@
+Twitter.configure do |config|
+  config.consumer_key = 7xKxgdC3pHcpHNyAV3BJg
+  config.consumer_secret = rwKC1IIM0t5xqKqZd41YVdwKmnV7tL2yQ70z3J8ugSc
+  config.oauth_token = 1491301814-53EGuYCKdNMpvt7BgoqDLZILfLN1aDsBKXZ4Tbv
+  config.oauth_token_secret = 1vDjxwgKNHNs988emcz9E3AYaH1YLEqzJb2avkb3I
+end
+
+Twitter.update("I'm telling on you!")
